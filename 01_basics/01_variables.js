e2c31a3775ddecbe4a65b35e48cb2b91f1746ee5
NITISH KUMAR <nikumar9771@gmail.com>
2026-06-24 10:17:01 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountId = 144553
+let accountEmail ="nitish@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+let accountState; // undefined hai
+
+
+// accountId = 2 // constant ko change nhi kr skte ye allow nhi hai
+
+
+accountemail = "raaj@gmail.com"
+accountPassword = "777777"
+accountCity = "Bihar"
+/*
+prefer not to use var
+because of issues in block scope and functional scope
+
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail,accountPassword,accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict" // treat all js code as newer version
+
+// alert(3 +3) // we are using nodejs, not browser
+
+console.log(3 + 3)  // code readability should be high
+console.log("Nitish")
+
+let name = "Nitish"
+let age = 18
+let isloggedIn = false
+let state;
+
+// number--> 2 to power 53
+// bigint
+// string --> ""
+// boolean --> true / false
+// null --> standalone value
+// undefined
+// symbol // uniqueckness find karne ke liye use krte hai
+
+// object
+
+console.log(typeof null) // object aata hai 
+console.log(typeof undefined)  //undefined aata h 
