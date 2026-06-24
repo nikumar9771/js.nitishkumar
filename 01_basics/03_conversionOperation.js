3bbe36a8a4ac30251096752afa887262f57c7c09
NITISH KUMAR <nikumar9771@gmail.com>
2026-06-24 12:03:13 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -12,7 +12,6 @@ let valueInNumber = Number(score)
 // true --> 1; false --> 0
 
 let isLoggedIn = 1
- let booleanIsloggedIn = Boolean(isLoggedIn)
 //console.log(booleanIsloggedIn);
 
 // 1 --> true; 0 --> false
@@ -21,4 +20,45 @@ let isLoggedIn = 1
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber)
+
+// ****************************** Operation ************************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " Nitish"
+let str3 = str1 + str2
+// console.log(str3);
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+num1 = num2 = num3 = num = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
