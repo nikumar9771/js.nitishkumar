434ffb58b97d54f12bada5da10a7e975da440217
NITISH KUMAR <nikumar9771@gmail.com>
2026-06-25 23:03:31 +0000

    string

---
+++ 01_basics/string.js
@@ -0,0 +1,35 @@
+const name = "Nitish"
+const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // `` backticks
+
+//const gameName = new String('Nitishk')
+// console.log(gameName[0]); //aise key values pair access krte hai
+// console.log(gameName.__proto__);// ptototype access krne ke liye
+// console.log(gameName.length); // use for lenth find
+// console.log(gameName.toUpperCase());  
+// console.log(gameName.charAt(3)); // use for konsa character kis position pe hai 
+// console.log(gameName.indexOf('t'));
+
+
+const gameName = new String('Nitish-k')
+console.log(gameName.charAt(3)); // use for konsa character kis position pe hai 
+ console.log(gameName.indexOf('t'));
+ const newString = gameName.substring(0, 4) //(startnumber, end num)
+console.log(newString);
+const anotherString = gameName.slice(-7, 4)
+console.log(anotherString);
+const newStringOne = "  Nitish  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+const url = "https://nitish.com/nitish%20kumar"
+console.log(url.replace('%20', '-'));
+console.log(url.includes('nitish'));
+
+
+
+
+
+
+
