8d6e3951b915ddb87c3d9ee92a1607528e23a062
NITISH KUMAR <nikumar9771@gmail.com>
2026-06-28 17:53:55 +0000

    string

--- 01_basics/string.js
+++ 01_basics/string.js
@@ -13,19 +13,20 @@ console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); // ``
 // console.log(gameName.indexOf('t'));
 
 
-const gameName = new String('Nitish-k')
-console.log(gameName.charAt(3)); // use for konsa character kis position pe hai 
- console.log(gameName.indexOf('t'));
- const newString = gameName.substring(0, 4) //(startnumber, end num)
-console.log(newString);
-const anotherString = gameName.slice(-7, 4)
-console.log(anotherString);
+const gameName = new String('Nitish_k_com')
+// console.log(gameName.charAt(3)); // use for konsa character kis position pe hai 
+//  console.log(gameName.indexOf('t'));
+//  const newString = gameName.substring(0, 4) //(startnumber, end num)
+// console.log(newString);
+// const anotherString = gameName.slice(-7, 4)
+// console.log(anotherString);
 const newStringOne = "  Nitish  "
-console.log(newStringOne);
+// console.log(newStringOne);
 console.log(newStringOne.trim());
-const url = "https://nitish.com/nitish%20kumar"
-console.log(url.replace('%20', '-'));
-console.log(url.includes('nitish'));
+// const url = "https://nitish.com/nitish%20kumar"
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('nitish'));  // pta karne ke liye use krte h ye name and word hai ya nhi
+console.log(gameName.split('_'));
 
 
 
