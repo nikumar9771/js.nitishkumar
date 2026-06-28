3de517990ebe3f6ba0c7fc651b8c240b1d412848
NITISH KUMAR <nikumar9771@gmail.com>
2026-06-28 19:18:14 +0000

    numAndMath

---
+++ 01_basics/numAndMath.js
@@ -0,0 +1,48 @@
+// const score = 400
+// console.log(score);
+
+
+// const balance = new Number(1000)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2)); //ecommerce application me bahut use hote hai percision value dekhne ke liye taki cusmober dekh skte 
+
+
+// const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(4));
+
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); // use for , (comma) like 1,000,000
+ 
+
+//++++++++++++++++++++++++++++++++++ Math +++++++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(2,3,4,5,6));
+// console.log(Math.max(2,3,4,5,6));
+
+
+
+console.log(Math.random());  // values aati hai between 0 to 1 
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min)
+
+
+
+
+
+
+
+
+
+
