ac55127f0ddfcc27229375a951aadfdd64c22e7e
NITISH KUMAR <nikumar9771@gmail.com>
2026-06-30 19:53:13 +0000

    02_array.js

--- Array/02_array.js
+++ Array/02_array.js
@@ -7,8 +7,30 @@ const dc_heros = ["superman", "flash", "batman"]
 // console.log(marvel_heros[3][1]);
 
 // concat method
-marvel_heros.concat(dc_heros)
-console.log(marvel_heros);
+// marvel_heros.concat(dc_heros)
+// console.log(marvel_heros);
 
+// const allHeros = marvel_heros.concat(dc_heros)
+//  console.log(allHeros);
+
+//  const all_new_heros = [...marvel_heros, ...dc_heros]
+//  console.log(all_new_heros);
+
+
+// const another_array = [1,2,3,[4,5,6], 7, [6, 7, [4,5]]] 
+// const real_another_array = another_array.flat(Infinity)
+// console.log(real_another_array);
+
+
+console.log(Array.isArray("Nitish"));
+console.log(Array.from("Nitish")); // form --> array me convert karta hai
+console.log(Array.from({name: "Nitish"})); // interview empty deta h for key 
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2, score3));
 
 
